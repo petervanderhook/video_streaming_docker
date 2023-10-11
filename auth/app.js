75165e3701c791ec2c89c5c3f979080d167164c1
petervanderhook <petervanderhook@gmail.com>
2023-10-10 23:57:47 -0700

    Added serving for streaming container. Fixed auth to completion.

--- auth/app.js
+++ auth/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 const users = {
   "tristan": "tristan",
@@ -7,34 +6,30 @@ const users = {
   "peter": "peter"
 };
 
-const authenticate = (req, res, next) => {
-  req.body = req.body || {};
-  const username = req.body.username;
-  const password = req.body.password;
-  if (!username || !password || users[username] !== password) {
-    return res.status(401).send("Unauthorized");
-  }
-  next();
-};
-
-app.use(function(req, res, next) {
-    if (req.method === 'POST' || req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });  
-
 app.use(express.json());
-app.use(authenticate);
+app.use(express.urlencoded({extended:false}))
 
-
-app.post("/", (req, res) => {
-  res.send("Welcome to the protected page!");
+// Serve the login page at the root ("/") route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
+// Handle form submission at the "/login" route
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (users[username] === password) {
+    res.redirect('/success')
+  } else {
+    res.redirect('/fail')
+  }
+});
+app.get("/fail", (req, res) => {
+  res.sendFile(__dirname + "/fail.html")
+})
+app.get("/success", (req, res) => {
+  res.sendFile(__dirname + "/images/garylarson.jpg");
+})
 
 app.listen(3000, () => {
   console.log("Auth service listening on port 3000");
